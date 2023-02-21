c688852edfc5f2d9027eecc0bf8b199dd354e4eb
Joel Hayag <hayagjoel@gmail.com>
2023-02-22 02:48:22 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,11 @@ use App\Http\Controllers\ShortLinkController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
+Route::get('/', [ShortLinkController::class,'index']);
 Route::get('generate-shorten-link', [ShortLinkController::class,'index']);
 Route::post('generate-shorten-link', [ShortLinkController::class,'store'])->name('generate.shorten.link.post');
 Route::get('{code}',[ShortLinkController::class,'shortenLink'])->name('shorten.link');
